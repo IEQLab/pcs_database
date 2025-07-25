8aec98b8a12bca1d77773d55b67d72c60bc2c47c
AkihisaNomoto <67300161+AkihisaNomoto@users.noreply.github.com>
2025-07-25 18:39:42 +0900

    Refactor `database_columns_names.py` to simplify functionality, align with updated body part constants, and improve CSV saving logic

--- code/data_processing/database_columns_names.py
+++ code/data_processing/database_columns_names.py
@@ -9,7 +9,7 @@ from config.configuration import Config
 from code.utils import utilities
 
 
-def generate_columns(body_parts=utilities.BodyPartTemporary):
+def generate_columns(body_parts=utilities.BodyPartLauraOutput):
     """
     Generate a list of column names for the dataset based on general columns, groups, and mannequin body parts.
     Returns:
@@ -41,51 +41,19 @@ def generate_columns(body_parts=utilities.BodyPartTemporary):
 
 
 def create_dataframe(columns):
-    """
-    Create an empty DataFrame with the specified columns and add a "Stability" column.
-
-    Args:
-        columns (list): List of column names for the DataFrame.
-    Returns:
-        DataFrame: Empty DataFrame with the specified columns.
-    """
+    """Create an empty DataFrame with the specified columns and add a "Stability" column."""
     df = pd.DataFrame(columns=columns)
     df["Stability"] = None  # Add Stability column at the end
     return df
 
 
-def save_dataframe_to_csv(df, file_path):
-    """
-    Save DataFrame to a CSV file, creating the directory if it doesn't exist.
-
-    Args:
-        df (DataFrame): The DataFrame to save.
-        file_path (str): Path to save the CSV file.
-    """
-    os.makedirs(
-        os.path.dirname(file_path), exist_ok=True
-    )  # Ensure the directory exists
-    df.to_csv(file_path, index=False)
-    print(f"DataFrame successfully saved to {file_path}")
-
-
 def main():
-    """
-    Main function to generate the DataFrame and save it as a CSV file.
-    """
-    # Generate all column names
-    columns = generate_columns(body_parts=utilities.BodyPartTemporary)
+    columns = generate_columns(body_parts=utilities.BodyPartLauraOutput)
+    df = create_dataframe(columns)  # Create an empty DataFrame with these columns
 
-    # For debugging/development purposes
-    # tmp_columns = generate_columns(body_parts=utilities.BodyPart)
-    # print(tmp_columns)
-
-    # Create an empty DataFrame with these columns
-    df = create_dataframe(columns)
-
-    # Define the output file path and save the DataFrame to a CSV file
-    output_file = os.path.join(Config.DataPaths.BASE_DIR, "columns_format.csv")
-    save_dataframe_to_csv(df, output_file)
+    path_to_save = os.path.join(Config.DataPaths.BASE_DIR, "columns_format.csv")
+    df.to_csv(os.path.join(path_to_save), index=False)
+    print(f"Columns format saved to {path_to_save}.")
 
 
 if __name__ == "__main__":
